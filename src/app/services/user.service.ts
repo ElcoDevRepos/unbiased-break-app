16390d60c21848ac93dcd3c04d11bc0ea69fead6
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-06-08 11:45:22 -0400

    Create service to get data for read articles

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, doc, collection, query, where, updateDoc, getDocs } from '@angular/fire/firestore';
+import { Firestore, doc, collection, query, where, updateDoc, getDocs, getDoc } from '@angular/fire/firestore';
 import {
   Auth,
   signInWithEmailAndPassword,
@@ -90,6 +90,60 @@ export class UserService {
     })
 
     return f;
+  }
+
+  async getReadArticles() {
+    if(!this.auth.currentUser) return;
+
+    let readArticles = [];
+    let ref = collection(this.firestore, 'users');
+    const q = query(ref, where('email', '==', this.auth.currentUser.email));
+    let docs = await getDocs(q);
+    docs.forEach((d) => {
+      const readArticlesIDs = d.data().readArticles.slice(0, 50);
+      readArticlesIDs.reverse();
+      let found = false;
+
+      readArticlesIDs.forEach(async id => {
+        // Query the middle-articles collection for documents with a matching ID
+        let q = query(collection(this.firestore, "middle-articles"), where("id", "==", id));
+        let querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          readArticles.push({
+            ...doc.data(),
+            type: 'middle-articles'
+          });
+          found = true;
+        });
+        
+        // Query the left-articles collection for documents with a matching ID
+        if(!found){
+          q = query(collection(this.firestore, "left-articles"), where("id", "==", id));
+          querySnapshot = await getDocs(q);
+          querySnapshot.forEach((doc) => {
+            readArticles.push({
+              ...doc.data(),
+              type: 'left-articles'
+            });
+            found = true;
+          });
+        }
+        
+        if(!found){
+          // Query the right-articles collection for documents with a matching ID
+          q = query(collection(this.firestore, "right-articles"), where("id", "==", id));
+          querySnapshot = await getDocs(q);
+          querySnapshot.forEach((doc) => {
+            readArticles.push({
+              ...doc.data(),
+              type: 'right-articles'
+            });
+          });
+        }
+      });            
+    });
+
+    return readArticles;
   }
 
   async setDeviceToken(token) {
