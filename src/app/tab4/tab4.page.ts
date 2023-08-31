2de8e4dfa0d65bf7d7d24165ea996fcf5b535b0f
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-08-31 10:21:49 -0400

    UI and query fix

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -9,13 +9,14 @@ import { Firestore, collection, query, where, orderBy, limit, startAfter, getDoc
 })
 export class Tab4Page implements OnInit{
 
-  gptSummaries = [];
+  gptSummaries;
 
   constructor( private tabsPage : TabsPage, private firestore : Firestore ) {}
 
   async ngOnInit() {
     try {
-      this.gptSummaries = await this.getSummaries();
+      let sum = await this.getSummaries();
+      this.gptSummaries = sum[0];
       console.log(this.gptSummaries); // This will print the summaries to the browser console
     } catch (error) {
       console.error("Failed to fetch summaries:", error);
@@ -29,15 +30,13 @@ export class Tab4Page implements OnInit{
   //Fetch the gpt summaries from firestore
   getSummaries(): Promise<any[]> {
     return new Promise((resolve, reject) => {
-      const now = new Date();
-      const twentyFourHoursAgo = new Date(now.getTime() - (24 * 60 * 60 * 1000));  // time in milliseconds
 
       const ref = collection(this.firestore, 'gpt-summaries');
-      const q = query(ref, where('timestamp', '<', twentyFourHoursAgo));
+      const q = query(ref, orderBy('timestamp', 'desc'), limit(1));
 
       getDocs(q)
         .then(docSnaps => {
-          resolve(docSnaps.docs.map(doc => doc.data()));
+          resolve(docSnaps.docs.map(doc => doc.data().summaries));
         })
         .catch(err => {
           reject(err);
