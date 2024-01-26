df62962009cf7e6fab97eccb6e6924e5e3db5cdb
olofholm <olof.fredrick.holmstrom@gmail.com>
2024-01-26 07:31:19 -0500

    Some basic UI

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TabsPage } from '../tabs/tabs.page';
+import { Auth } from '@angular/fire/auth';
+import { Firestore, collection, doc, getDoc, getDocs, limit, orderBy, query, updateDoc, where } from '@angular/fire/firestore';
+import { Browser } from '@capacitor/browser';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-tab5',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab5Page implements OnInit {
 
-  constructor() { }
+  feed = [];
+
+  constructor(
+    private tabsPage: TabsPage,
+    private firestore: Firestore,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.loadCommunityFeedArticles().then(() => {
+
+    });
   }
 
+  ionViewWillEnter() {
+    this.tabsPage.selectedTab = 'tab5';
+  }
+
+  // Get articles from firestore
+  async loadCommunityFeedArticles() {
+    const responsesRef = collection(this.firestore, 'community-feed');
+    let q = query(
+      responsesRef,
+      orderBy('timestamp', 'desc'),
+    );
+    let docSnaps = await getDocs(q);
+
+    docSnaps.forEach((d) => {
+      this.feed.push(d.data());
+    });
+  }
+
+  onArticleClick(item: any) {
+    const link = item.link;
+    if (link.includes('nytimes.com') || link.includes('wsj.com')) {
+      this.addToRead(item.id); //Add the wsj or nyt article to read now because it won't be opening the article page
+      Browser.open({ url: link });
+    }
+  }
+
+  async addToRead(articleId) {
+    if (!this.userService.getLoggedInUser()) return;
+    let user = await getDoc(
+      doc(this.firestore, 'users', this.userService.getLoggedInUser().uid)
+    );
+    let readArticles = user.data().readArticles || [];
+
+    if (!readArticles.includes(articleId)) readArticles.push(articleId);
+
+    updateDoc(
+      doc(this.firestore, 'users', this.userService.getLoggedInUser().uid),
+      {
+        readArticles,
+      }
+    );
+  }
 }
